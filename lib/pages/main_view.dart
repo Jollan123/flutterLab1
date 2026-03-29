51ff5867c4c682ba15619820036f5b1b0677321f
Jolina Jedborn <jolina@jedborn.se>
2026-03-29 19:11:00 +0200

    första commit

---
+++ lib/pages/main_view.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:lab1/widgets/task_list.dart';
+
+class MainView extends StatelessWidget {
+  const MainView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+             title: const Text('Things todo'),
+             backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          ),
+    body: TaskList(
+    children: [
+     ListTile(title: Text('DAT216/TIG091')),
+     ListTile(title: Text('Lab1 Todos')),
+     ListTile(title: Text('En rad till')),
+      ],
+    ),
+  );
+}
+}
